41231e517f856285f02c92cd9390fa5c33bf2620
Ian Vergel <cianvergelll@gmail.com>
2025-02-17 14:13:39 +0800

    basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,11 @@ class LoginPageState extends State<LoginPage> {
   final TextEditingController _passwordController = TextEditingController();
 
   void _login() {
-    if (_emailController.text == 'cianvergelll@gmail.com' && _passwordController.text == 'ian123') {
+    // Hardcoded credentials
+    const String correctEmail = 'cianvergelll@gmail.com';
+    const String correctPassword = 'ian123';
+
+    if (_emailController.text == correctEmail && _passwordController.text == correctPassword) {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => const HomePage()),
@@ -47,54 +51,120 @@ class LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black26,
-                blurRadius: 10,
-                spreadRadius: 2,
-                offset: Offset(2, 5),
-              ),
-            ],
+      body: Column(
+        children: [
+          // Top 2/3 of the Screen: Image with Gradient and Title/Subtitle
+          Expanded(
+            flex: 2,
+            child: Stack(
+              children: [
+                Image.asset(
+                  'assets/it_guy.jpg',
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  height: double.infinity,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                      colors: [
+                        Colors.white.withAlpha(229),
+                        Colors.white.withAlpha(51),
+                      ],
+                    ),
+                  ),
+                ),
+                const Positioned(
+                  left: 20,
+                  bottom: 40,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'CodeShelf:',
+                        style: TextStyle(
+                          fontSize: 40, // Larger font size for CodeShelf
+                          fontWeight: FontWeight.bold,
+                          color: Colors.deepPurple,
+                        ),
+                      ),
+                      Text(
+                        'Your Infinite IT Library',
+                        style: TextStyle(
+                          fontSize: 30, // Smaller font size for the second part
+                          color: Colors.deepPurple,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'Unlock the world of IT knowledge—anytime, anywhere.',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.deepPurple,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-          width: 300,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(),
+          // Bottom 1/3 of the Screen: Login Form
+          Expanded(
+            flex: 1,
+            child: Align(
+              alignment: Alignment.topCenter, // Align form to top
+              child: Container(
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 10,
+                      spreadRadius: 2,
+                      offset: const Offset(2, 5),
+                    ),
+                  ],
+                ),
+                width: 300,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextField(
+                      controller: _emailController,
+                      decoration: const InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    TextField(
+                      controller: _passwordController,
+                      decoration: const InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(),
+                      ),
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: _login,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.deepPurple,
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+                      ),
+                      child: const Text('Login', style: TextStyle(color: Colors.white)),
+                    ),
+                  ],
                 ),
               ),
-              const SizedBox(height: 10),
-              TextField(
-                controller: _passwordController,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
-                ),
-                obscureText: true,
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _login,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.deepPurple,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-                ),
-                child: const Text('Login', style: TextStyle(color: Colors.white)),
-              ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
       backgroundColor: Colors.grey[200],
     );
@@ -104,11 +174,62 @@ class LoginPageState extends State<LoginPage> {
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  void _logout(BuildContext context) {
+    // Navigate back to the LoginPage
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const LoginPage()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Book App Home')),
-      body: const Center(child: Text('Welcome to the Book App!')),
+      appBar: AppBar(
+        title: const Text('Book App Home'),
+        actions: [
+          // Favorites section at the right of the AppBar
+          IconButton(
+            icon: const Icon(
+              Icons.favorite,
+              color: Colors.red,
+              size: 30,
+            ),
+            onPressed: () {
+              // This can be a future feature for navigating to a Favorites page
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Welcome message in the center of the screen
+            const Center(
+              child: Text(
+                'Welcome to the Book App!',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            // Log Out Button
+            ElevatedButton(
+              onPressed: () => _logout(context),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.deepPurple,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+              ),
+              child: const Text('Log Out', style: TextStyle(color: Colors.white)),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
